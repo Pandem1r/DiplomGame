c8165ee679ae851c81944c601b4b4ad89a1cb6da
Никита Горчинский <92267602+Pandem1r@users.noreply.github.com>
2024-04-18 01:36:38 +0300

    Initial commit

---
+++ Source/Diplom/Diplom.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Diplom.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Diplom, "Diplom" );
